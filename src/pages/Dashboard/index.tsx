5028bc8dadcceb51acd2155d819fe46b047f901b
Welber Marques <welbih@yahoo.com.br>
2021-03-24 09:15:12 -0300

    desafio concluido

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -35,14 +35,15 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO
+      const response = await api.get('/products');
+      setProducts(response.data);
     }
 
     loadProducts();
-  }, []);
+  }, [setProducts]);
 
   function handleAddToCart(item: Product): void {
-    // TODO
+    addToCart(item);
   }
 
   return (
